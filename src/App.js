e284c499559c1f26886d4316f011b6cd322a1c8e
Takenotsuka <pavane90@gmail.com>
2020-03-03 22:23:09 +0900

    .map Recap

--- src/App.js
+++ src/App.js
@@ -1,33 +1,33 @@
 import React from "react";
-
-function Food({ name, image }) {
-  return (
-    <div>
-      <h1>I like {name}</h1>
-      <img src={image}></img>
-    </div>
-  );
-}
-
 const foodILike = [
   {
+    id: 1,
     name: "kimchi",
     image:
       "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi.ytimg.com%2Fvi%2FeTucCw1w6Ak%2Fmaxresdefault.jpg&f=1&nofb=1"
   },
   {
+    id: 2,
     name: "kimbob",
     image:
       "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi.ytimg.com%2Fvi%2FY-Y9CXGRJPU%2Fmaxresdefault.jpg&f=1&nofb=1"
   }
 ];
 
+function Food({ name, image }) {
+  return (
+    <div>
+      <h1>I like {name}</h1>
+      <img src={image} alt={name}></img>
+    </div>
+  );
+}
+
 function App() {
   return (
     <div>
       {foodILike.map(dish => (
-        <Food name={dish.name} image={dish.image} />
-        // 프로퍼티의 이름을 그대로 컴포넌트에 보낼 수 있다.
+        <Food key={dish.id} name={dish.name} image={dish.image}></Food>
       ))}
     </div>
   );
