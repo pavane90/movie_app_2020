c63a9600d148d514b88299b5296f2f122d1a8eee
Takenotsuka <pavane90@gmail.com>
2020-03-08 17:28:01 +0900

    Class Components and State

--- src/App.js
+++ src/App.js
@@ -1,50 +1,25 @@
 import React from "react";
 import PropTypes from "prop-types";
-const foodILike = [
-  {
-    id: 1,
-    name: "kimchi",
-    image:
-      "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi.ytimg.com%2Fvi%2FeTucCw1w6Ak%2Fmaxresdefault.jpg&f=1&nofb=1"
-  },
-  {
-    id: 2,
-    name: "kimbob",
-    image:
-      "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi.ytimg.com%2Fvi%2FY-Y9CXGRJPU%2Fmaxresdefault.jpg&f=1&nofb=1",
-    rating: 4.3
+
+class App extends React.Component {
+  state = {
+    count: 0
+  };
+  add = () => {
+    console.log("add");
+  };
+  minus = () => {
+    console.log("minus");
+  };
+  render() {
+    return (
+      <div>
+        <h1>The number is : {this.state.count}</h1>
+        <button onClick={this.add}>Add</button>
+        <button onClick={this.minus}>Minus</button>
+      </div>
+    );
   }
-];
-
-function Food({ name, image, rating }) {
-  return (
-    <div>
-      <h1>I like {name}</h1>
-      <h4>{rating}/5.0</h4>
-      <img src={image} alt={name}></img>
-    </div>
-  );
-}
-
-Food.propTypes = {
-  name: PropTypes.string.isRequired,
-  image: PropTypes.string.isRequired,
-  rating: PropTypes.number
-};
-
-function App() {
-  return (
-    <div>
-      {foodILike.map(dish => (
-        <Food
-          key={dish.id}
-          name={dish.name}
-          image={dish.image}
-          rating={dish.rating}
-        ></Food>
-      ))}
-    </div>
-  );
 }
 
 export default App;
